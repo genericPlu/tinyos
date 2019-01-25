8ee1afaff4b3787cb85efd772eb22b57b1776b30
pluguezadam <pluguezadamp@gmail.com>
2019-01-25 11:22:07 -0800

    Change

--- TestSim.py
+++ TestSim.py
@@ -138,13 +138,12 @@ def main():
     s.addChannel(s.COMMAND_CHANNEL);
     s.addChannel(s.GENERAL_CHANNEL);
     s.addChannel(s.FLOODING_CHANNEL);
+	s.addChannel(s.NEIGHBOR_CHANNEL);
 	
-    s.runTime(20);
-    s.ping(1, 2, "Hello, World");
-    s.runTime(10);
-    s.ping(1, 3, "Hi!");
-    s.runTime(20);
-    s.ping(1, 19, "Hi!");
+	for i in range(20):
+		s.runTime(20);
+		s.ping(i, i+1, "Hello"); 
+	
     s.runTime(20);
     s.neighborDMP(1);
     s.runTime(20);
