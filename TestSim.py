cb5e81260b6788aecf57880ef19ebcdaac574a59
pluguezadam <pluguezadamp@gmail.com>
2019-01-29 19:26:41 -0800

    Change

--- TestSim.py
+++ TestSim.py
@@ -137,6 +137,12 @@ def main():
     s.bootAll();
     s.addChannel(s.COMMAND_CHANNEL);
     s.addChannel(s.GENERAL_CHANNEL);
+	
+	s.runTime(20);
+    s.ping(1, 2, "Hello, World");
+    s.runTime(10);
+    s.ping(1, 3, "Hi!");
+    s.runTime(20);
 
 	
     
