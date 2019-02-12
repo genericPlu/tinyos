a75ca9089530f145d90cd32f6e237b8e9f3d8c69
pluguezadam <pluguezadamp@gmail.com>
2019-02-12 04:42:40 -0800

    m

--- TestSimCopy.py
+++ TestSimCopy.py
@@ -143,9 +143,9 @@ def main():
     s.runTime(20);
     s.ping(1, 19, 1); 
     #for i in range(1,20):
-		#s.runTime(50);
-		#s.neighborDMP(i);
-		#s.runTime(50);
+	#s.runTime(50);
+	#s.neighborDMP(i);
+	#s.runTime(50);
 	s.runTime(50);
 	s.neighborDMP(i);
 	s.runTime(50);
