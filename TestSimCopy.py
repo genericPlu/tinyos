bbb51d82721be3c9b17a180706f0bf4270eb6200
pluguezadam <pluguezadamp@gmail.com>
2019-02-12 04:43:35 -0800

    m

--- TestSimCopy.py
+++ TestSimCopy.py
@@ -143,12 +143,12 @@ def main():
     s.runTime(20);
     s.ping(1, 19, 1); 
     #for i in range(1,20):
-	#s.runTime(50);
-	#s.neighborDMP(i);
-	#s.runTime(50);
-	s.runTime(50);
-	s.neighborDMP(i);
-	s.runTime(50);
+        #s.runTime(50);
+        #s.neighborDMP(i);
+        #s.runTime(50);
+    s.runTime(50);
+    s.neighborDMP(i);
+    s.runTime(50);
 	
 if __name__ == '__main__':
     main()
